b2ac0565480777588338f5ac666da9808494c6e4
maxi <maxigomez.udaondo@gmail.com>
2023-10-08 11:22:20 -0300

    api

--- pages/HomePage.jsx
+++ pages/HomePage.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react"
-import *as TrabajosServices from '/services/trabajos.services.js'
+import *as TrabajosServices from '../services/trabajos.services.js'
 import emailjs from '@emailjs/browser';
 import React, { useRef } from 'react';
 import { Link } from "react-router-dom";
@@ -141,7 +141,7 @@ function HomePage(){
         <section id="trabajos">
           <h2>TRABAJOS REALIZADOS</h2>
           <div className="contenedor-trabajos">
-          {trabajos.map(({_id, nombre, portada}) =>
+          {trabajos.map(({_id, portada}) =>
           <article key={_id} className="contenedor-trabajos">
             <Link to={`/trabajos/${_id}`}>
                 <img src={`../assets/${portada}`}></img>
--- services/trabajos.services.js
+++ services/trabajos.services.js
@@ -1,5 +1,5 @@
 async function find() {
-    return fetch('http://localhost:2022/api/trabajos')
+    return fetch('https://arieldiezsolucioneselectricas.vercel.app/api/trabajos')
         .then(response => {
             if (response.ok) {
                 return response.json()
